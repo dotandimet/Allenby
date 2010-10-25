4088e90bdadea77a15c6f4e7b02175cc01a77328
Dotan Dimet <dotan@corky.net>
2010-10-26 01:00:57 +0200

    added slide/show controller, css, .gitignore and .perltidyrc

---
+++ lib/Allenby/Slide.pm
@@ -0,0 +1,13 @@
+package Allenby::Slide;
+
+use strict;
+use warnings;
+
+use base 'Mojolicious::Controller';
+
+# This action will render a template
+sub show {
+
+};
+
+1;
---
+++ public/js/allenby.js
@@ -0,0 +1,6 @@
+$(
+    function(){
+        $('.sorter').sortable();
+        $('.sorter').disableSelection();
+   }
+);
